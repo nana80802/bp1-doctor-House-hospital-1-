e7e67daba684bfe1a7c0ecbfa9c20c430f4a7251
nana80802 <miss_nisreen2000@yahoo.com>
2018-04-15 14:29:16 +0200

    Add Spring structure for Nursery & Accountancy

--- diagnose/src/main/java/at/refugeescode/diagnose/endpoint/DiagnoseEndPoint.java
+++ diagnose/src/main/java/at/refugeescode/diagnose/endpoint/DiagnoseEndPoint.java
@@ -2,22 +2,31 @@ package at.refugeescode.diagnose.endpoint;
 
 import at.refugeescode.diagnose.controller.DrHouse;
 import at.refugeescode.diagnose.model.Patient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/patients")
 public class DiagnoseEndPoint {
- private DrHouse drHouse;
 
-    public DiagnoseEndPoint(DrHouse drHouse) {
+    private DrHouse drHouse;
+
+    private RestTemplate restTemplate;
+
+    @Value("${nursery.url}")
+    private String nurseryUrl;
+
+    public DiagnoseEndPoint(DrHouse drHouse, RestTemplate restTemplate) {
         this.drHouse = drHouse;
+        this.restTemplate = restTemplate;
     }
 
     @PostMapping
-    void getPatient(@RequestBody Patient patient){
+    void getPatient(@RequestBody Patient patient) {
         Patient drHouseDiagnoses = drHouse.diagnose(patient);
         System.out.println(drHouseDiagnoses);
     }
